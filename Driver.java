9639f807e4821ad30889c27bf065ec85e2b88750
mitulbhalani95 <101879746+mitulbhalani95@users.noreply.github.com>
2022-06-12 12:20:58 +0530

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,32 @@
+package com.mitul.q1.driver;
+
+import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+import com.mitul.q1.service.Service;
+
+class Driver {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("enter the total no of floors in the building");
+		
+		int noOfFloor = sc.nextInt();
+
+	    int[] floors = new int[noOfFloor];
+		
+	   
+		for(int i=0; i<noOfFloor; i++)  
+		{  
+		System.out.println("enter the floor size given on day : "+(i+1));
+		floors[i] = sc.nextInt(); 
+		}  
+		System.out.println();
+		
+		Service service = new Service();
+		service.printConstructionTable(floors, noOfFloor);
+	}
+}
